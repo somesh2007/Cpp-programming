a2a22289721ee8cde9dae833e61f217beb0a71ba
somesh2007 <kalyankarsomesh00@gmail.com>
2026-08-21 23:17:32 +0530

    Add initial C++ learning structure

---
+++ Basics/AreaofTriangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float Area,Height,Base;
+    cout<<"Enter the height of triangle: ";
+    cin>>Height;
+    cout<<"Enter the Base of triangle: ";
+    cin>>Base;
+    Area=(Base*Height)/2;
+    cout<<"Area of Triangle: "<<Area;
+
+}
---
+++ README.md.txt
@@ -0,0 +1,8 @@
+# C++ Learning
+
+This repository contains my C++ learning journey.
+
+## Topics
+
+- C++ Basics
+- Arrays
