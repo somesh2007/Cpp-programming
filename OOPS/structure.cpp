f5a208510e36df0dd39e2a313cef2c0e85517758
somesh2007 <kalyankarsomesh00@gmail.com>
2026-09-04 23:18:03 +0530

    Added OOPS concepts

---
+++ OOPS/structure.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct Demo{
+    int x;
+    int y;
+
+    void Display(){
+        cout<<x<<" "<<y<<endl;
+    }
+
+
+};
+
+int main(){
+    Demo d;
+    d.x=10;
+    d.y=20;
+    d.Display();
+}
